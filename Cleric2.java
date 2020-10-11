0dfce3fc21d25eacebeaaa6076df92f69e35561a
toru <t02f447f@gmail.com>
2020-10-12 05:58:16 +0900

    9.5練習問題終了

--- Cleric2.java
+++ Cleric2.java
@@ -5,14 +5,40 @@ import java.util.Random;
 public class Cleric2 {
 	String name;
 	int hp = 50;
-	final int MaxHp = 50;
 	int mp = 10;
-	final int MaxMp = 10;
+	static final int MAX_HP = 50;
+	static final int MAX_MP = 10;
+	
+	public Cleric2(String name, int hp, int mp) {
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;
+	}
+	
+//	public Cleric2(String name, int hp) {
+//		this.name = name;
+//		this.hp = hp;
+//		this.mp = Cleric2.MAX_MP;
+//	}
+	// コンストラクタを使ってコンストラクタを定義
+	public Cleric2(String name, int hp) {
+		this(name, hp, Cleric2.MAX_MP);
+	}
+	
+//	public Cleric2(String name) {
+//		this.name = name;
+//		this.hp = Cleric2.MAX_HP;
+//		this.mp = Cleric2.MAX_MP;
+//	}
+	//　更に上記のコンストラクタを使ってコンストラクタを定義
+	public Cleric2(String name) {
+		this(name, Cleric2.MAX_HP);
+	}
 	
 	public void selfAid() {
 		System.out.println(this.name + "はセルフエイドを唱えた！");
 		this.mp -= 5;
-		this.hp = this.MaxHp;
+		this.hp = Cleric2.MAX_HP;
 		System.out.println("HPを最大まで回復した！");
 	}
 	
@@ -32,7 +58,7 @@ public class Cleric2 {
 		int recover = new Random().nextInt(3) + count;
 		
 		// 実際の回復量を乱数を用いて決定する
-		int recoverActual = Math.min(this.MaxMp - this.mp, recover);
+		int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
 		
 		this.hp += recoverActual;
 		System.out.println("MPが" + recoverActual + "回復した。");
