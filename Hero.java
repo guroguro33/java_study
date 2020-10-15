5b18fa1c6241a2e16ca2f88fd84063f2e588c52c
toru <t02f447f@gmail.com>
2020-10-16 05:51:45 +0900

    10-2終了

---
+++ Hero.java
@@ -0,0 +1,32 @@
+package jp.learning.javastudy;
+
+public class Hero {
+	private int hp;
+	private int mp;
+	private String name;
+	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getHp() {
+		return this.hp;
+	}
+	
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+	
+	public int getMp() {
+		return this.mp;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+}
---
+++ Wizard.java
@@ -0,0 +1,48 @@
+package jp.learning.javastudy;
+
+public class Wizard {
+	private int hp;
+	private int mp;
+	private String name;
+	private Wand wand;
+	
+	public void heal(Hero h) {
+		int basePoint = 10; // 基本回復ポイント
+		int recoverPoint = (int) (basePoint * this.getWand().getPower()); // 杖による増幅
+		h.setHp(h.getHp() + recoverPoint); // 勇者のHPを回復させる
+		System.out.println(h.getName() + "のHPを" + recoverPoint + "回復した！");
+	}
+	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getHp() {
+		return this.hp;
+	}
+	
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+	
+	public int getMp() {
+		return this.mp;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public void setWand(Wand wand){
+		this.wand = wand;
+	}
+
+	public Wand getWand(){
+		return this.wand;
+	}
+	
+}
