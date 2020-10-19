b8fbab0b81bd1e5cfa282cfdf78776b4dc583db0
toru <t02f447f@gmail.com>
2020-10-20 05:59:00 +0900

    java12章練習問題12-1途中

---
+++ Book.java
@@ -0,0 +1,14 @@
+package jp.learning.javastudy;
+
+public class Book extends TangibleAsset{
+    private String isbn;
+    // コンストラクタ
+    public Book(String name, int price, String color, String isbn){
+        super(name, price, color);
+        this.isbn = isbn;
+    }
+    // getterメソッド
+    public String getIsbn(){
+        return this.isbn;
+    }
+}
